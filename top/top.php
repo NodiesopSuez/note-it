be6f8bd70dda64857abc3248b837aed169bd31c3
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-03-14 20:15:00 +0900

    setting colors for gradetion

--- top/top.php
+++ top/top.php
@@ -10,11 +10,14 @@ require_once('../class/config/Config.php');
 <html lang="en">
 <head>
     <?php include('../head.php')?>
+    <link rel="stylesheet" type="text/css" href="../main/template.css">
     <link rel="stylesheet" type="text/css" href="./top.css">
 </head>
 <body>
-    <div class="catch_logo">
-         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Config::LOGO_MARK?></svg>
+    <div class="container">
+        <div class="catch_logo">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Config::LOGO_MARK?></svg>
+        </div>
     </div>
 
 
