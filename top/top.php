f148135534c278f85cafef9f0a8e9ac6944cf073
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-03-15 08:34:12 +0900

    mark up elements

--- top/top.php
+++ top/top.php
@@ -4,10 +4,14 @@ session_regenerate_id();
 
 //外部ファイル読込
 require_once('../class/config/Config.php');
+
+$menus = ['news'=>'blue','story'=>'pink','feature'=>'yellow', 'Q & A'=>'green'];
+$img = './img/ladybug_nm.png';
+$msg = 'ようこそ♪'; 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <?php include('../head.php')?>
     <link rel="stylesheet" type="text/css" href="../main/template.css">
@@ -17,7 +21,23 @@ require_once('../class/config/Config.php');
     <div class="container">
         <div class="catch_logo">
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Config::LOGO_MARK?></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 81.8 22.6"><?=Config::LOGO_TYPE?></svg>
+            <p>紙も時間も無駄にして困り果てた<br/>初心者プログラマーがつくったクラウドノート</p>
         </div>
+        <div class="sign_nav">
+            <button><p>sign in</p></button>
+            <div>
+                <img src="<?= $img ?>">
+                <p><?= $msg ?></p>
+            </div>            
+            <button><p>sign in</p></button>
+        </div>
+        <div class="note_nav">
+            <?php foreach($menus as $menu => $color): ?>
+             <div class="<?= $color ?>"><?= $menu ?></div>
+            <?php endforeach?>
+        </div>
+        
     </div>
 
 
