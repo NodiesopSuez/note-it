600bdf8b4f4641799bcc42e3e4c76f564bb9a02d
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-03-16 16:32:55 +0900

    add temporary styles for top-section

--- top/top.php
+++ top/top.php
@@ -5,7 +5,7 @@ session_regenerate_id();
 //外部ファイル読込
 require_once('../class/config/Config.php');
 
-$menus = ['news'=>'blue','story'=>'pink','feature'=>'yellow', 'Q & A'=>'green'];
+$menus = ['news'=>'n_blue','story'=>'n_pink','feature'=>'n_yellow', 'Q & A'=>'n_green'];
 $img = './img/ladybug_nm.png';
 $msg = 'ようこそ♪'; 
 ?>
@@ -19,24 +19,29 @@ $msg = 'ようこそ♪';
 </head>
 <body>
     <div class="container">
-        <div class="catch_logo">
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Config::LOGO_MARK?></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 81.8 22.6"><?=Config::LOGO_TYPE?></svg>
-            <p>紙も時間も無駄にして困り果てた<br/>初心者プログラマーがつくったクラウドノート</p>
-        </div>
-        <div class="sign_nav">
-            <button><p>sign in</p></button>
-            <div>
-                <img src="<?= $img ?>">
-                <p><?= $msg ?></p>
-            </div>            
-            <button><p>sign in</p></button>
-        </div>
-        <div class="note_nav">
-            <?php foreach($menus as $menu => $color): ?>
-             <div class="<?= $color ?>"><?= $menu ?></div>
-            <?php endforeach?>
-        </div>
+        <section class="top">
+            <div class="catch_logo">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Config::LOGO_MARK?></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 81.8 22.6"><?=Config::LOGO_TYPE?></svg>
+                <p>紙も時間も無駄にして困り果てた<br/>初心者プログラマーがつくったクラウドノート</p>
+            </div>
+            <div class="sign_nav">
+                <button><p>sign in</p></button>
+                <div>
+                    <img src="<?= $img ?>">
+                    <p><?= $msg ?></p>
+                </div>            
+                <button><p>sign up</p></button>
+            </div>
+            <div class="note_nav">
+                <?php foreach($menus as $menu => $color): ?>
+                <div class="note <?= $color ?>">
+                    <p><?= $menu ?></p>
+                </div>
+                <?php endforeach?>
+            </div>
+        </section>
+        
         
     </div>
 
