c300b6c8e62e5f6f29b86f9536524d95bcf26c10
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-03 19:53:27 +0900

    ノート〜コンテンツ削除するメソッド記述

--- class/db/Deletes.php
+++ class/db/Deletes.php
@@ -7,26 +7,87 @@ class Deletes extends Connect
         parent::__construct();
     }
 
+    //ノートを削除
+    public function deleteNote(int $note_id):bool{
+        $sql = "DELETE FROM note_info WHERE note_id = :note_id";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->bindValue('note_id', $note_id, PDO::PARAM_INT);
+        $bool = $stmt->execute();
+
+        return $bool;
+    }
+
+    //チャプターを削除
+    public function deleteChapter(string $designation, $id):bool{
+        //$designation = 'note' or 'chapter'
+        $sql = "DELETE FROM chapter_info WHERE ". $designation ."_id = ". $id;
+        $stmt = $this->dbh->prepare($sql);
+        $bool = $stmt->execute();
+
+        return $bool;
+    }
+
+
+
+    //page_infoとpage_contentsから削除
+    public function deletePageContents(string $designation, int $page_type, $id_list){
+        //ノート・チャプターから削除orページから削除 / どちらのページタイプで削除するか
+        if ($designation === 'page') {
+            if ($page_type === 1) {
+                $sql_contents = "DELETE FROM page_a_contents WHERE page_id = ". $id_list ;
+            } elseif ($page_type === 2) {
+                $sql_contents = "DELETE FROM page_b_contents WHERE page_id = ". $id_list;
+            }
+            $sql_page = "DELETE FROM page_info WHERE page_id = ". $id_list;
+        } elseif ($designation === 'note_chapter'){
+            if ($page_type === 1){
+                $sql_contents = "DELETE FROM page_a_contents USING page_info 
+                                 WHERE page_a_contents.page_id = page_info.page_id 
+                                 AND page_info.chapter_id IN (". $id_list .")";
+            }elseif($page_type === 2){
+                $sql_contents = "DELETE FROM page_b_contents USING page_info 
+                                 WHERE page_b_contents.page_id = page_info.page_id 
+                                 AND page_info.chapter_id IN (". $id_list .")";
+            }
+            $sql_page = "DELETE FROM page_info WHERE chapter_id IN (". $id_list .")";
+        }
+
+        //先にコンテンツから削除
+        $stmt_contents = $this->dbh->prepare($sql_contents);
+        $bool_contents = $stmt_contents->execute();
+
+        $stmt_page = $this->dbh->prepare($sql_page);
+        $bool_page = $stmt_page->execute();
+
+        $bool = ($bool_contents === true) && ($bool_page === true) ? true : false ;
+
+        return $bool;
+    }
+
+
+    //typeBで登録してい画像ファイルを削除
     public function removeFiles(string $designation, $id_list):bool{
         if($designation === 'note_chapter'){
             $sql = "SELECT * FROM page_b_contents 
                     INNER JOIN page_info ON page_b_contents.page_id = page_info.page_id 
                     WHERE page_info.chapter_id IN (" . $id_list .")";
         }elseif($designation === 'page'){
-            $sql = "SELECT * FROM pageb_contents WHERE page_id = ". $id_list ;
+            $sql = "SELECT * FROM page_b_contents WHERE page_id = ". $id_list ;
         }
         
         $stmt = $this->dbh->prepare($sql);
         $stmt->execute();
         $get_info = $stmt->FetchAll(PDO::FETCH_ASSOC);
 
+
+        $unlink_bool = array();
         foreach($get_info as $info){
             if($info['file_type'] === 'img'){
                 $unlink_bool[] = unlink($info['data']);
             }
         }
 
-        $bool = in_array(0, $unlink_bool) ? false : true ;
+        $bool = empty($unlink_bool) || !in_array(0, $unlink_bool) ? true : false ;
         return $bool;
     }
 }
--- note_chapter/delete_note.php
+++ note_chapter/delete_note.php
@@ -37,13 +37,16 @@ try{
     $delete = new Deletes;
 
     $chapter_list = $search->findChapterInfo($note_id); //チャプターリスト
+    var_dump($chapter_list);
+    $page_a = array();
+    $page_b = array();
     foreach($chapter_list as $chapter_id => $val){
         $val['page_type'] == 1 ? $page_a[] = $chapter_id : null; //typeAのチャプター
         $val['page_type'] == 2 ? $page_b[] = $chapter_id : null; //typeBのチャプター
     }
 
     //SQLへ渡す用にstring型へ
-    if(!empty($page_a) && count($page_a) > 1){
+/*     if(!empty($page_a) && count($page_a) > 1){
         $page_a_str = implode(',', $page_a);
     }elseif(!empty($page_a) && count($page_a) === 1) {
         $page_a_str = implode($page_a);
@@ -53,25 +56,63 @@ try{
     }elseif(!empty($page_b) && count($page_b) === 1) {
         $page_b_str = implode($page_b);
     }
+ */
+
+    //chapter_idをString型にしてページとコンテンツ削除
+    if(!empty($page_a)){
+        if(count($page_a) > 1){
+            $page_a_str = implode(',', $page_a);
+        }elseif(count($page_a) === 1){
+            $page_a_str = implode($page_a);
+        }
+        $delete_bool['page_a'] = $delete->deletePageContents('note_chapter', 1, $page_a_str);
+    }
+
+    if(!empty($page_b)){
+        if(count($page_b) > 1){
+            $page_b_str = implode(',', $page_b);
+        }elseif(count($page_b) === 1){
+            $page_b_str = implode($page_b);
+        }
+        $delete_bool['page_b_file'] = $delete->removeFiles('note_chapter', $page_b_str);  //サーバ上の画像ファイル削除
+        $delete_bool['page_b']      = $delete->deletePageContents('note_chapter', 2, $page_b_str);
+    }
+
+    //チャプターを削除
+    $delete_bool['chapter'] = $delete->deleteChapter('note', $note_id);
+    
+    //ノートを削除
+    $delete_bool['note'] = $delete->deleteNote($note_id);
 
     echo '<br/><br/>page_a<br/>';
-    var_dump($page_a);
-    echo $page_a_str;
+    if (isset($page_a) && !empty($page_a)) {
+        var_dump($page_a);
+        echo $page_a_str;
+    }
     echo '<br/><br/>page_b<br/>';
-    if (isset($page_b)) {
+    if (isset($page_b) && !empty($page_b)) {
         var_dump($page_b);
         echo $page_b_str;
     }
 
-    //typeBの画像ファイルを削除
-    if(isset($page_b) && count($page_b) >=1){
-        $remove_files = $delete->removeFiles('note_chapter', $page_b_str);
-        var_dump($remove_files);
+    print_r($delete_bool);
+
+    if(in_array(0, $delete_bool)){
+        $_SESSION['error'][] = Config::MSG_EXCEPTION;
+        header('Location:../page/create_page.php');
+        exit;
+    }else{
+        $_SESSION['okmsg'][] = 'ノートを削除できました！';
+        header('Location:../page/create_page.php');
+        exit;
+        
     }
 
-
 }catch(Exception $e){
-
+    echo $e->getMessage();
+    $_SESSION['error'][] = Config::MSG_EXCEPTION;
+    header('Location:../page/create_page.php');
+    exit;
 }
 
 ?>
