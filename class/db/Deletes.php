59174f85d419bc7459293e0bc0b1e455a831b115
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-03 17:10:52 +0900

    removeFiles追加

--- class/db/Deletes.php
+++ class/db/Deletes.php
@@ -7,13 +7,26 @@ class Deletes extends Connect
         parent::__construct();
     }
 
-    public function removeFiles(string $designation, string $file_list):bool{
-        if($designation ==='note_chapter'){
+    public function removeFiles(string $designation, $id_list):bool{
+        if($designation === 'note_chapter'){
             $sql = "SELECT * FROM page_b_contents 
                     INNER JOIN page_info ON page_b_contents.page_id = page_info.page_id 
-                    WHERE page_info.chapter_id IN (" . $file_list .")";
-        } 
-        $bool = true;
+                    WHERE page_info.chapter_id IN (" . $id_list .")";
+        }elseif($designation === 'page'){
+            $sql = "SELECT * FROM pageb_contents WHERE page_id = ". $id_list ;
+        }
+        
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute();
+        $get_info = $stmt->FetchAll(PDO::FETCH_ASSOC);
+
+        foreach($get_info as $info){
+            if($info['file_type'] === 'img'){
+                $unlink_bool[] = unlink($info['data']);
+            }
+        }
+
+        $bool = in_array(0, $unlink_bool) ? false : true ;
         return $bool;
     }
 }
