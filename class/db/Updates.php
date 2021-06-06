b402fbfa02bd1f168f028c81424bda45e2c27f42
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-06 15:10:46 +0900

    チャプター名を更新するメソッド追加

--- class/db/Updates.php
+++ class/db/Updates.php
@@ -7,6 +7,7 @@ class Updates extends Connect
         parent::__construct();
     }
 
+    //ノート情報を更新
     public function updateNote($note_id, $color, $note_title):bool{
         $sql  = "UPDATE note_info SET note_title = :note_title, color = :color WHERE note_id = :note_id";
         $stmt = $this->dbh->prepare($sql);
@@ -17,5 +18,17 @@ class Updates extends Connect
 
         return $bool;
     }
+
+    //チャプター情報を更新
+    public function updateChapter($chapter_id, $chapter_title):bool{
+        $sql = "UPDATE chapter_info SET chapter_title = :chapter_title WHERE chapter_id = :chapter_id";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->bindValue(':chapter_id', $chapter_id, PDO::PARAM_STR);
+        $stmt->bindValue(':chapter_title', $chapter_title, PDO::PARAM_STR);
+        $bool = $stmt->execute();
+
+        return $bool;
+    }
+
 }
 ?>
---
+++ note_chapter/edit_chapter_done.php
@@ -0,0 +1,57 @@
+<?php
+//セッションスタート
+session_start();
+session_regenerate_id();
+
+//必要ファイル呼び出し
+require_once('../class/config/Config.php');
+require_once('../class/util/Utility.php');
+require_once('../class/db/Connect.php');
+require_once('../class/db/Users.php');
+require_once('../class/db/Searches.php');
+require_once('../class/db/Updates.php');
+
+/* //ログインしてなければログイン画面に
+if(empty($_SESSION['user_info'])){
+    header('Location: ../sign/sign_in.php');
+    exit;
+}
+
+//ワンタイムトークンチェック
+if(!SaftyUtil::validToken($_SESSION['token'])){
+	$_SESSION['error'][] = Config::MSG_INVALID_PROCESS;
+	header('Location: ../sign/sign_in.php');
+	exit;
+} */
+
+//エラーが入ってたら削除
+$_SESSION['error'] = array();
+
+$user_id = 4; //$_SESSION['user_info']['user_id'];
+extract($_SESSION['note_chapter']); //[chapter_id, chapter_title];
+var_dump($chapter_id);
+
+try {
+    $update = new Updates;
+    $update_bool = $update->updateChapter($chapter_id, $chapter_title);
+
+    if($update_bool === false){
+        $_SESSION['error'][] = Config::MSG_EXCEPTION;
+        header('Location:../mem/mem_top.php'); //エラーがあったら入力ページに戻る
+        exit;
+    }else{
+        $_SESSION['okmsg'][] = 'チャプターの更新ができました！';
+        $_SESSION['note_chapter'] = array();
+        header('Location:../mem/mem_top.php'); 
+        exit;
+    }
+
+    $update = null;
+
+}catch(Exception $e){
+    echo $e->getMessage();
+    $_SESSION['error'][] = Config::MSG_EXCEPTION;
+    header('Location:../mem/mem_top.php');
+    exit;
+}
+?>
