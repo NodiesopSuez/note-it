adab8b25808df094d9030c7db65d446ea935c833
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-14 01:05:35 +0900

    linearGradientのカラー

--- top/top.php
+++ top/top.php
@@ -114,54 +114,64 @@ $_SESSION['okmsg'] = array();
         </section>
         <section class="story">
             <img src="./img/header_story.png">
-            <div class="first_contents">
-                <h1>DEVELOPER'S DESIRE</h1>
-                <h1>OF SELF-SOlVING</h1>
-                <h1>IS THE BEGINNING</h1>
-                <p class="p_bold">「抱えている問題を自分のつくるプログラムで解決してみたい」<br/>
-                    そんな私の挑戦心がnote ITのはじまり。</p>
-            </div>  
-            <div class="second_contents">
-                <img src="./img/story_1.png">
-                <p>プログラマーとして活躍するべく、学習を始めた私。<br/>
-                    学習した内容を忘れないためにも、<br/>
-                    都度その内容をノートに書き留めていました。</p>
-            </div> 
-            <div class="third_contents">
-                <p>学習を進めたい気持ちはありつつも<br/>
-                    「綺麗にノートを書きたい」という<br/>
-                    こだわりを諦められず…</p>
-                <p>一文字でも失敗したと感じたら<br/>
-                    新しいページへ書き直す…<br/>
-                    この繰り返しにより、<br/>
-                    私は紙も時間も無駄にしていました。</p>
-                <img src="./img/story_2.png">
-            </div> 
-            <div class="forth_contents">
-                <div class="balloon">
-                    <p>自分でクラウドノートを作れば<br/>
-                        プログラミングの勉強にもなるし、<br/>
-                        ほんの少しでも<br/>
-                        同じような悩みを抱える人の<br/>
-                        手助けになるかもしれない…</p>
-                    <img src="./img/bubble_lg.png">
-                    <img src="./img/bubble_md.png">
-                    <img src="./img/bubble_sm.png"> 
+            <div class="story_frame">
+                <div class="first_contents">
+                    <h1>DEVELOPER'S DESIRE</h1>
+                    <h1>OF SELF-SOlVING</h1>
+                    <h1>IS THE BEGINNING</h1>
+                    <p class="p_bold">「抱えている問題を自分のつくるプログラムで解決してみたい」<br/>
+                        そんな私の挑戦心がnote ITのはじまり。</p>
+                </div>  
+                <div class="second_contents">
+                    <img src="./img/story_1.png">
+                    <p>プログラマーとして活躍するべく、学習を始めた私。<br/>
+                        学習した内容を忘れないためにも、<br/>
+                        都度その内容をノートに書き留めていました。</p>
+                </div> 
+                <div class="third_contents">
+                    <p>学習を進めたい気持ちはありつつも<br/>
+                        「綺麗にノートを書きたい」という<br/>
+                        こだわりを諦められず…</p>
+                    <p>一文字でも失敗したと感じたら<br/>
+                        新しいページへ書き直すを繰り返して…<br/>
+                        紙も時間も無駄にしていました。</p>
+                    <img src="./img/story_2.png">
+                </div> 
+                <div class="forth_contents">
+                    <div class="balloon">
+                        <p>自分でクラウドノートを作れば<br/>
+                            プログラミングの勉強にもなるし、<br/>
+                            ほんの少しでも<br/>
+                            同じような悩みを抱える人の<br/>
+                            手助けになるかもしれない…</p>
+                        <img src="./img/bubble_lg.png">
+                        <img src="./img/bubble_md.png">
+                        <img src="./img/bubble_sm.png"> 
+                    </div>
+                    <img src="./img/story_3.png">
+                    <p>この「ある日の思いつき」が<br/>
+                        note ITは生まれました。</p>
                 </div>
-                <img src="./img/story_3.png">
-                <p>この「ある日の思いつき」が<br/>
-                    note ITは生まれました。</p>
-            </div>
-            <div class="fifth_contents">
-                <img src="./img/story_4.png">
-                <h1>IMPROVING OF DEVELOPER'S SKILL<br/>
-                    ADD MORE FEATURES OF 'note IT'</h1>
-                <p class="p_bold">私がプログラマーとして成長し続ける限り、note ITも成長し続けます。</p>
-                <p>初心者プログラマーがつくったクラウドノートなので<br/>
-                    世に出ているノートアプリに比べて、昨日もデザインもまだまだ。<br/>
-                    私のスキルが成長するとともに、note ITも成長します。<br/>
-                    ご利用される方におきましてはご不便をかけるかもしれませんが、<br/>
-                    暖かく見守りながら使っていただけると幸いです。</p>
+                <div class="fifth_contents">
+                    <img src="./img/story_4.png">
+                    <h1>IMPROVING OF DEVELOPER'S SKILL<br/>
+                        ADD MORE FEATURES OF 'note IT'</h1>
+                    <p class="p_bold">私がプログラマーとして成長し続ける限り、note ITも成長し続けます。</p>
+                    <p>初心者プログラマーがつくったクラウドノートなので<br/>
+                        世に出ているノートアプリに比べて、昨日もデザインもまだまだ。<br/>
+                        私のスキルが成長するとともに、note ITも成長します。<br/>
+                        ご利用される方におきましてはご不便をかけるかもしれませんが、<br/>
+                        暖かく見守りながら使っていただけると幸いです。</p> 
+                </div>
+                <a class="note blue lets_signin" href="../sign/sign_up.php">
+                    <div class="note_base"></div>
+                    <div class="note_title">
+                        <p>Let's<br/>
+                        Sign Up!!</p>
+                    </div>
+                    <div class="back_cover"></div>
+                </a>
+                <p>さぁ、ノートを作ってみましょう！</p>
             </div>
         </section>
     </div>
