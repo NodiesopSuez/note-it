1e14414f1162597271f4e999f4f5d6685098177a
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-13 17:20:46 +0900

    buttoin要素を一旦コメントアウト

--- inclusion/top_header.php
+++ inclusion/top_header.php
@@ -3,12 +3,12 @@
 ?>
 
 
-<header>
+<header class="blue">
     <nav>
-    <button>news</button>
+<!--     <button>news</button>
     <button>story</button>
     <button>feature</button>
-    <button>Q&A</button>
+    <button>Q&A</button> -->
     <a class="top" href="../top/top.php">
     <svg class="logo_mark" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Icons::LOGO_MARK?></svg>
     </a>
