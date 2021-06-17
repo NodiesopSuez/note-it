e2b9dc4fbbfc80cc4eed5afc3b9c8bf46a015a2f
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-17 17:38:35 +0900

    headerメニューの順番を逆順に修正

--- inclusion/mem_header.php
+++ inclusion/mem_header.php
@@ -5,10 +5,10 @@
 
 <header class="<?= $color ?>">
     <nav>
-    <button>news</button>
-    <button>Q&A</button>
-    <button>account</button>
-    <button>sign out</button>
+        <button>Q&A</button>
+        <button>sign out</button>
+        <button>account</button>
+        <button>news</button>
     <a class="to_top" href="../mem/mem_top.php">
         <svg class="logo_mark" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Icons::LOGO_MARK?></svg>
     </a>
--- inclusion/top_header.php
+++ inclusion/top_header.php
@@ -5,12 +5,12 @@
 
 <header class="basic">
     <nav>
-        <button>news</button>
-        <button>story</button>
-        <button>feature</button>
         <button>Q&A</button>
-    <a class="to_top" href="../top/top.php">
-    <svg class="logo_mark" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Icons::LOGO_MARK?></svg>
-    </a>
+        <button>feature</button>
+        <button>story</button>
+        <button>news</button>
+        <a class="to_top" href="../top/top.php">
+            <svg class="logo_mark" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 66.9 66.62"><?=Icons::LOGO_MARK?></svg>
+        </a>
     </nav>
 </header>
