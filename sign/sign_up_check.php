01669e92bc45bb4bd1fb1a794c50268d76bff9d5
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-28 15:59:58 +0900

    DBにつなげてバリデーションできているか確認するための出力追加

--- sign/sign_up_check.php
+++ sign/sign_up_check.php
@@ -33,31 +33,31 @@ $user_info = $db->findUserInfo($email, $category);
 $db = null;
 
 //メールアドレス NG >> 空欄・半角でない・@入力ない・同一アドレス存在
-if($email == '' || mb_ereg_match('^(\s|　)+$',$email)){
+/* if($email == '' || mb_ereg_match('^(\s|　)+$',$email)){
 	$_SESSION['error'][] = 'メールアドレスを入力してください。';
 }elseif(preg_match("|^[a-z0-9_./?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$|",$email)==false){
 	$_SESSION['error'][] = 'メールアドレスを正しく入力してください。';
 }elseif(!empty($user_info)){
 	$_SESSION['error'][] = '既に同一アドレスでのご登録がされています。';
-}
+} */
 
 //ネーム NG >> 空欄
-if($nick_name == '' || mb_ereg_match('^(\s|　)+$',$nick_name)){
+/* if($nick_name == '' || mb_ereg_match('^(\s|　)+$',$nick_name)){
 	$_SESSION['error'][] = 'ニックネームを入力してください。';
-}
+} */
 
 //性別 NG >> 未選択
-if(!isset($gender)){
+/* if(!isset($gender)){
     $_SESSION['error'][] = '性別を選んでください。';
-}
+} */
 
 //生年月日 NG >> 未選択
-if($birth == '' || mb_ereg_match('^(\s|　)+$',$birth)){
+/* if($birth == '' || mb_ereg_match('^(\s|　)+$',$birth)){
 	$_SESSION['error'][] = '生年月日を入力してください。';
-}
+} */
 
 //パスワード NG >> 1回目空欄・2回目空欄・不一致・半角もしくは8文字以上でない
-if($pass == '' || mb_ereg_match('^(\s|　)+$',$pass)){
+/* if($pass == '' || mb_ereg_match('^(\s|　)+$',$pass)){
 	$_SESSION['error'][] = 'パスワードを入力してください。';
 }elseif($pass2 == '' || mb_ereg_match('^(\s|　)+$',$pass2)){
 	$_SESSION['error'][] = '２回目のパスワードを入力してください。';
@@ -75,7 +75,7 @@ if(
 	}
 	
 echo 'エラーは<br/>';
-print_r($_SESSION);
+print_r($_SESSION); */
 
 //$_SESSIONにエラーメッセージが含まれていたら、登録画面に戻る
 if(!empty($_SESSION['error'])){
