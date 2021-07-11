3e428af4003fd3a8a5d5600616ba113b53eab84b
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-11 17:52:14 +0900

    インデックスで取得した要素をクローンできるか

--- page/edit_page.js
+++ page/edit_page.js
@@ -18,14 +18,12 @@ $(function(){
 
         //1個目のtextフォームブロックを複製して後ろに挿入
         //let new_form_block = $('#form_block_1').clone().attr({ id : `form_block_${new_form_count}` }).insertBefore('.add_contents.row'); 
-        let new_form_block = $('.text').parent()[0];
-        console.log(new_form_block);
-        //let new_form_block = $('.text').parent().clone().attr({ id : `form_block_${new_form_count}` }).insertBefore('.add_contents.row'); 
+/*         let new_form_block = $('.text').parent()[0];
+        console.log(new_form_block); */
+        let new_form_block = $('.text').parent()[0].clone().attr({ id : `${new_form_count}_form_block` }).insertBefore('.add_contents.row'); 
         
         //フォームブロック内の要素のidとテキストを書き換え
         $(new_form_block).children('#contents_1').attr({ id : `contents_${new_form_count}`, name : `contents_${new_form_count}`}).text('');
-        $(new_form_block).append(delete_btn);
-        /* ¥¥$(new_form_block).children('#hid_contents_1').attr({ name : `contents_${new_form_count}`, id : `hid_contents_${new_form_count}`}); */
     });
 
     //add_img_btnをクリック → 画像選択ウィンドウ表示
