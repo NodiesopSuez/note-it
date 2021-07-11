0200e2ee65427f45889f5321cddc7ed7a625a5fe
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-11 17:30:36 +0900

    数値型へのキャストをparseFloatに変更

--- page/edit_page.js
+++ page/edit_page.js
@@ -16,7 +16,7 @@ $(function(){
     $(document).on("click", '#add_text', function(){
         //let new_form_count = $('.form_block').length + 1; //新フォームブロックは何個目か
         let form_count = $('.page_base.b').children().eq(-2).attr('id')/* .replace('form_block_', '') */;
-        let new_form_count = parseFloat(form_count)+1;
+        let new_form_count = parseFloat(form_count);
         console.log(new_form_count);
 
         //1個目のフォームブロックを複製して後ろに挿入
