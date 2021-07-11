887b2a69d2a2838ef6ac7f083c865bfdfd39cf65
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-11 18:01:31 +0900

    add_imgでの#form_block指定修正

--- page/edit_page.js
+++ page/edit_page.js
@@ -59,12 +59,12 @@ $(function(){
         
         //FileReadeerに対応しているか
         if(window.FileReader){
-            $(`#form_block_${set_form_num}`).children().remove();
+            $(`#${set_form_num}_form_block`).children().remove();
             let fileReader = new FileReader();
             fileReader.onload = function(){
                 //表示サムネイル,表示画像を選択した画像に切替
                 let img_thumb = $('<img>').attr({ id : `thumb_${set_id}`, src : fileReader.result });
-                $(`#form_block_${set_form_num}`).prepend(img_thumb, edit_btn, delete_btn);
+                $(`${set_form_num}_form_block`).prepend(img_thumb, edit_btn, delete_btn);
             }
             fileReader.readAsDataURL(selected_file);
         }else{
