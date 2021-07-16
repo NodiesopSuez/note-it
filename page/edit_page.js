fce2f9fa7687420ee8131d1462aaa38b5514680e
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-16 15:56:24 +0900

    $_FILESテスト

--- page/edit_page.js
+++ page/edit_page.js
@@ -63,7 +63,7 @@ $(function(){
             fileReader.onload = function(){
                 //表示サムネイル,表示画像を選択した画像に切替
                 let img_thumb = $('<img>').attr({ id : `thumb_${set_form_num}`, src : fileReader.result });
-                $(`#${set_form_num}_form_block`).prepend(img_thumb, $(edit_btn).clone().attr({ for : `contents_${set_form_num}` }), $(delete_btn).clone());
+                $(`#${set_form_num}_form_block`).prepend(img_thumb, $(edit_btn).clone().attr({ for : `contents_${set_form_num}[img]` }), $(delete_btn).clone());
             }
             fileReader.readAsDataURL(selected_file);
         }else{
