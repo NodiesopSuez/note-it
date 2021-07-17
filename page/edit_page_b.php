d30a160b4f3fa86df79e4d75ddf967841ad8e7c1
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-17 15:44:41 +0900

    $_SESSION[page]の格納記述を修正

--- page/edit_page_b.php
+++ page/edit_page_b.php
@@ -38,7 +38,7 @@ try {
     //ページとコンテンツ情報
     $get_page_info = $search->findPageContentsB($page_id);
     $page_info     = $utility->sanitize(2, $get_page_info['page']);
-    $_SESSION['page']['page_id'] = $page_info['page_id'];
+    $_SESSION['page'] = ['page_id' => $page_info['page_id']];
 
     for($i=0 ; $i<count($get_page_info['contents']) ; $i++ ){
         $page_contents[] = $utility->sanitize(2, $get_page_info['contents'][$i]);
