1b5a652291cdfe85ae269bacacf722b14a955be1
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-27 18:10:11 +0900

    改行追加

--- class/config/Config.php
+++ class/config/Config.php
@@ -6,7 +6,7 @@
         const MSG_INVALID_PROCESS = '不正な処理が行われました。';
         
         //例外がスローされた時
-        const MSG_EXCEPTION  = '申し訳ございません。エラーが発生しました。';
+        const MSG_EXCEPTION  = '申し訳ございません。<br/>エラーが発生しました。';
         
         //ログイン試行回数オーバー
         const MSG_USER_LOGIN_TRYTIMES_OVER = 'ログインできません。<br/>しばらくしてから再度ログインしてください。';
