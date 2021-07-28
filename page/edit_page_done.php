bdc343673353d0ae90cc8fdff94eb3070a599a1d
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-28 21:29:14 +0900

    DB更新できたらmem_topへ遷移

--- page/edit_page_done.php
+++ page/edit_page_done.php
@@ -21,34 +21,32 @@ try{
     $update_contents = ['contents'   => $_SESSION['page']['update_contents'], 
                         'page_id'    => $_SESSION['page']['page_id'], 
                         'page_title' => $_SESSION['page']['page_title']];
-    print_r($update_contents);
-    echo '<br/><br/>';
-    print_r($_SESSION['page']);
 
     $update = new Updates;
 
     if($_SESSION['page']['page_type'] === 1){
         $update_contents_done = $update->updatePageContentsA($update_contents);
-        echo $update_contents_done;
-        //header('Location:../mem/mem_top.php');
-        exit;
     }elseif($_SESSION['page']['page_type'] === 2){
-        print_r($update_contents);
         $update_contents_done = $update->updatePageContentsB($update_contents);
-        echo $update_contents_done; 
-        $_SESSION['msg'] = ['error' => ['ページを更新できました!']];
-        //header('Location:../mem/mem_top.php');
-        exit;
     }else{
         $_SESSION['msg'] = ['error' => [Config::MSG_EXCEPTION]];
-        //header('Location:../mem/mem_top.php');
+        header('Location:../mem/mem_top.php');
         exit;
     }
+
+    if($update_contents_done === true){
+        $_SESSION['msg'] = ['okmsg' => ['ページを更新できました!']];
+    }else{
+        $_SESSION['msg'] = ['error' => [Config::MSG_EXCEPTION]];
+    }
+
+    header('Location:../mem/mem_top.php');
+    exit;
     
 }catch(Exception $e){
     echo $e->getMessage();
     $_SESSION['msg'] = ['error' => [Config::MSG_EXCEPTION]];
-    //header('Location:../mem/mem_top.php');
+    header('Location:../mem/mem_top.php');
     exit;
 }
 
