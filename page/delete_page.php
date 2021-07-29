b7b55086b6dc983c2d21855b9b9f3f7df001e57f
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-29 15:15:10 +0900

    $_SESSION[page]出力

--- page/delete_page.php
+++ page/delete_page.php
@@ -32,25 +32,9 @@ if(!empty($_SESSION['page'])){
 }
 
 try {
-    $search  = new Searches;
-    $utility = new SaftyUtil;
+    print_r($_SESSION['page']);
 
-    //ページとコンテンツ情報
-    $get_page_info = $search->findPageContentsA($page_id);
-    $page_info     = $utility->sanitize(2, $get_page_info);
-    //チャプター情報
-    $chapter_id   = $page_info['chapter_id'];
-    $chapter_info = $search->findChapterInfo('chapter_id', $chapter_id);
-    //ノート情報
-    $note_id   = $chapter_info[$chapter_id]['note_id'];
-    $note_info = $search->findNoteInfo('note_id', $note_id);
-
-    $note_title = $utility->sanitize(4, $note_info[$note_id]['note_title']);
-    $color      = $utility->sanitize(4, $note_info[$note_id]['color']);
-    $chapter_title = $utility->sanitize(4, $chapter_info[$chapter_id]['chapter_title']);
-
-    $search = null;
-
+    
 }catch(Exception $e){
     $_SESSION['msg'] = ['error' => [Config::MSG_EXCEPTION]];
     header('Location:../mem/mem_top.php');
