dcf5aef3be8f293f7456fc2ec2661cdeac401fd9
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-30 15:02:15 +0900

    ディレクトリ構成修正

---
+++ common/redirect.php
@@ -0,0 +1,29 @@
+<?php
+//セッションスタート
+session_start();
+session_regenerate_id();
+
+//必要ファイル呼び出し
+require_once(dirname(__FILE__, 2).'/class/config/Config.php');
+require_once(dirname(__FILE__, 2).'/class/config/Icons.php');
+require_once(dirname(__FILE__, 2).'/class/db/Connect.php');
+require_once(dirname(__FILE__, 2).'/class/db/Searches.php');
+require_once(dirname(__FILE__, 2).'/class/util/Utility.php');
+
+//ログインしてなければログイン画面に
+if(empty($_SESSION['user_info'])){
+    header('Location: ../sign/sign_in.php');
+    exit;
+}
+
+//ワンタイムトークンチェック
+if(!SaftyUtil::validToken($_SESSION['token'])){
+	$_SESSION['msg'] = ['error' => [Config::MSG_INVALID_PROCESS]];
+	header('Location: ../sign/sign_in.php');
+	exit;
+}
+
+
+
+
+?>
