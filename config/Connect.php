7b5d30e2d65b2ce550dbfe1e47aee578d9aa6b86
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-30 14:38:14 +0900

    ディレクトリ構成を修正

--- config/Connect.php
+++ config/Connect.php
@@ -1,16 +1,5 @@
 <?php
 class Connect {
-    /* const DB_NAME = 'note_it';
-    const DB_HOST = 'localhost';
-    const DB_USER = 'yamiyo_tomoshibi';
-    const DB_PASS = 'n0.d1esop_suez';
-    protected $dbh;
-    public function __construct(){
-        $dsn = 'pgsql:dbname='.self::DB_NAME.';host='.self::DB_HOST.';port=5432';
-        $this->dbh = new PDO($dsn,self::DB_USER,self::DB_PASS);
-        $this->dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    } */
-    
     protected $dbh;
 
     public function __construct(){
