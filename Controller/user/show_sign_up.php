0d3c4febfc4fc91e1dcc8dfe449fa8bbc2b970d9
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-07-31 14:43:41 +0900

    サーバー側の処理をControllerに分ける

---
+++ Controller/user/show_sign_up.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+session_regenerate_id();
+
+//外部ファイル読込
+require_once(dirname(__FILE__, 2).'/config/Config.php');
+require_once(dirname(__FILE__, 2).'/util/Utility.php');
+
+$email = '';
+$nick_name = '';
+$birth = '';
+$gender= '';
+$ladybug = './img/ladybug_nm.png';
+$msg = ['情報を入力してください。'];
+
+//エラーあるか
+if(!empty($_SESSION['msg']['error'])){
+    $ladybug = './img/ladybug_sd.png';
+    extract($_SESSION['data']);
+    $msg = $_SESSION['msg']['error'];
+}
+
+$show_msg = count($msg)>=2 ? implode("<br/>", $msg) : $msg[0];
+
+//前回入力時の値を表示
+function showPrevContents($contents){
+    if(!empty($contents)){
+        echo 'value="'.$contents.'"';
+    }
+}
+function showPrevChoice($choice){
+    if(!empty($gender) && $gender==$choice){
+        echo 'checked="checked"';
+    }
+}
+
+?>
--- Views/user/sign_up.php
+++ Views/user/sign_up.php
@@ -1,38 +1,6 @@
 <?php
-session_start();
-session_regenerate_id();
 
-//外部ファイル読込
- require_once(dirname(__FILE__, 2).'/class/config/Config.php');
- require_once(dirname(__FILE__, 2).'/class/util/Utility.php');
-
-$email = '';
-$nick_name = '';
-$birth = '';
-$gender= '';
-$ladybug = './img/ladybug_nm.png';
-$msg = ['情報を入力してください。'];
-
-//エラーあるか
-if(!empty($_SESSION['msg']['error'])){
-    $ladybug = './img/ladybug_sd.png';
-    extract($_SESSION['data']);
-    $msg = $_SESSION['msg']['error'];
-}
-
-$show_msg = count($msg)>=2 ? implode("<br/>", $msg) : $msg[0];
-
-//前回入力時の値を表示
-function showPrevContents($contents){
-    if(!empty($contents)){
-        echo 'value="'.$contents.'"';
-    }
-}
-function showPrevChoice($choice){
-    if(!empty($gender) && $gender==$choice){
-        echo 'checked="checked"';
-    }
-}
+include(dirname(__FILE__, 2).'/Controller/page/show_sign_up.php');
 
 ?>
 
