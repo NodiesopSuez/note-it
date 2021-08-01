c41e944f4dc4c6d18d748b6a2669b858c923d0ee
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-01 14:36:22 +0900

    セッションスタート、requireをredirect.phpのincludeに置き換え

--- Controller/user/show_sign_up.php
+++ Controller/user/show_sign_up.php
@@ -1,11 +1,6 @@
 <?php
 
-session_start();
-session_regenerate_id();
-
-//外部ファイル読込
-require_once(dirname(__FILE__, 2).'/config/Config.php');
-require_once(dirname(__FILE__, 2).'/util/Utility.php');
+include(dirname(__FILE__, 2).'/common/redirect.php');
 
 $email = '';
 $nick_name = '';
