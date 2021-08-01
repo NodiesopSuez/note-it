36c65ed0f620d9b92032565741f81ae3a15cb320
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-01 14:31:25 +0900

    認証、トークンチェックを関数定義

--- common/redirect.php
+++ common/redirect.php
@@ -8,16 +8,20 @@ require_once(dirname(__FILE__, 2).'/config/Config.php');
 require_once(dirname(__FILE__, 2).'/util/Utility.php');
 
 //ログインしてなければログイン画面に
-if(empty($_SESSION['user_info'])){
-    header('Location: ../sign/sign_in.php');
-    exit;
+function cannnotAuthenticate(){
+    if (empty($_SESSION['user_info'])) {
+        header('Location: ../Views/user/sign_in.php');
+        exit;
+    }
 }
 
 //ワンタイムトークンチェック
-if(!SaftyUtil::validToken($_SESSION['token'])){
-	$_SESSION['msg'] = ['error' => [Config::MSG_INVALID_PROCESS]];
-	header('Location: ../sign/sign_in.php');
-	exit;
+function validToken(){
+    if(!SaftyUtil::validToken($_SESSION['token'])){
+        $_SESSION['msg'] = ['error' => [Config::MSG_INVALID_PROCESS]];
+        header('Location: ../Views/user/sign_in.php');
+        exit;
+    }
 }
 
 function catchException(){
