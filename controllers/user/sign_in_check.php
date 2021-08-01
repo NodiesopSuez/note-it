e7dd5b1adebf67bfe1437a0152ddd99d8cdf17b2
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-01 19:13:04 +0900

    パス修正

--- controllers/user/sign_in_check.php
+++ controllers/user/sign_in_check.php
@@ -5,8 +5,8 @@ include(dirname(__FILE__, 3).'/common/redirect.php');
 //ワンタイムトークンチェック
 validToken();
 
-//必要ファイル呼び出し
-require_once(dirname(__FILE__, 3).'/models/Connect.php');
+//外部ファイル読込
+require_once(dirname(__FILE__, 3).'/config/Connect.php');
 require_once(dirname(__FILE__, 3).'/models/Users.php');
 
 //エラ〜メッセージを空にする
