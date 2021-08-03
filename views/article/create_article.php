f97070e7986a5771dda4522c977f620732d948fa
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-03 23:51:06 +0900

    includeの階層指定修正

--- views/article/create_article.php
+++ views/article/create_article.php
@@ -7,7 +7,7 @@ include(dirname(__FILE__, 3).'/controllers/article/show_create_article.php');
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <?php include(dirname(__FILE__, 2).'/head.php')?>
+    <?php include(dirname(__FILE__, 3).'/head.php')?>
     <link rel="stylesheet" type="text/css" href="/public/css/template.css">
     <link rel="stylesheet" type="text/css" href="/public/css/color_template.css">
     <link rel="stylesheet" type="text/css" href="/public/css/page.css" media="screen and (min-width:1024px)">
