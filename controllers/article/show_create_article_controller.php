59b8b68d32d2312b596907c128e53d985efc949d
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-04 00:47:25 +0900

    メッセージと画像を格納する変数名をshuuse

--- controllers/article/show_create_article_controller.php
+++ controllers/article/show_create_article_controller.php
@@ -17,16 +17,16 @@ $note_list = $searches->findNoteInfo('user_id', $user_id);
 
 //エラーの有無によってテントウの表示を分岐
 if(!empty($_SESSION['msg']['error'])){
-    $_SESSION['ladybug_img'] = '/public/public/img/ladybug_sd.png';
+    $ladybug_img = '/public/public/img/ladybug_sd.png';
     $msg = $_SESSION['msg']['error'];
 }elseif(!empty($_SESSION['msg']['okmsg'])){
-    $_SESSION['ladybug_img'] = '/public/public/img/ladybug_nm.png';
+    $ladybug_img = '/public/public/img/ladybug_nm.png';
     $msg = $_SESSION['msg']['okmsg'];
 }else{
-    $_SESSION['ladybug_img'] = '/public/public/img/ladybug_nm.png';
+    $ladybug_img = '/public/public/img/ladybug_nm.png';
     $msg = ['どのノートに追加しますか？'];
 }
-$msg = count($msg)>=2 ? implode("<br/>", $msg) : $msg[0];
+$show_msg = count($msg)>=2 ? implode("<br/>", $msg) : $msg[0];
 
 $color = 'basic'; //ヘッダーメニューのカラークラス
 $color_list = ['blue', 'pink', 'yellow', 'green', 'purple'];
