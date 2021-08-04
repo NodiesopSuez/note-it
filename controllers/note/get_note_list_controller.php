4d01d6ae379bd1836242aa27ab7ec280c8b89413
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-04 16:35:38 +0900

    requireの階層指定修正

--- controllers/note/get_note_list_controller.php
+++ controllers/note/get_note_list_controller.php
@@ -1,8 +1,8 @@
 <?php
 include(dirname(__FILE__, 3).'/common/redirect.php');
 
-require_once(dirname(__FILE__, 2).'/config/Connect.php');
-require_once(dirname(__FILE__, 2).'/models/Searches.php');
+require_once(dirname(__FILE__, 3).'/config/Connect.php');
+require_once(dirname(__FILE__, 3).'/models/Searches.php');
 
 try {
     header('Content-Type: application/json; charset=UTF-8');
