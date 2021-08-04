950d6176a268e19ee3c9f420b055aab2329419f8
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-04 17:24:46 +0900

    遷移先パス修正

--- controllers/article/create_article_check_controller.php
+++ controllers/article/create_article_check_controller.php
@@ -163,9 +163,9 @@ try {
     $search = null;
 
     if(!empty($_SESSION['msg'])){
-        header('Location:/article/create_article.php'); //エラーがあったら入力ページに戻る
+        header('Location:/views/article/create_article.php'); //エラーがあったら入力ページに戻る
     }else{
-        header('Location:/article/create_article_done_controller.php');
+        header('Location:/controllers/create_article_done_controller.php');
     }
 
 }catch(Exception $e){
