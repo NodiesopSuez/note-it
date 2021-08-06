4e4fefa272f7fbcabaa5abb917771636a505516c
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-06 20:26:40 +0900

    エラーメッセージの格納を修正

--- controllers/user/sign_up_check_controller.php
+++ controllers/user/sign_up_check_controller.php
@@ -8,11 +8,7 @@ validToken();
 require_once(dirname(__FILE__, 3).'/config/Connect.php');
 require_once(dirname(__FILE__, 3).'/models/Users.php');
 
-
-//エラー削除
-if(!empty($_SESSION['msg']['error'])){
-    $_SESSION = array();
-}
+unset($_SESSION['msg']);
 
 try{
 //受け取った情報を変数に代入
@@ -27,35 +23,35 @@ $user_info = $users->findUserInfo($email, $category);
 
 //メールアドレス NG >> 空欄・半角でない・@入力ない・同一アドレス存在
 if($email == '' || mb_ereg_match('^(\s|　)+$',$email)){
-	$_SESSION['msg'] = ['error' => ['メールアドレスを入力してください。']];
+	$_SESSION['msg']['error'][] = 'メールアドレスを入力してください。';
 }elseif(preg_match("|^[a-z0-9_./?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$|",$email)==false){
-	$_SESSION['msg'] = ['error' => ['メールアドレスを正しく入力してください。']];
+	$_SESSION['msg']['error'][] = 'メールアドレスを正しく入力してください。';
 }elseif(!empty($user_info)){
-	$_SESSION['msg'] = ['error' => ['既に同一アドレスで登録されています。']];
+	$_SESSION['msg']['error'][] = '既に同一アドレスで登録されています。';
 }
 
 //ネーム NG >> 空欄
 if($nick_name == '' || mb_ereg_match('^(\s|　)+$',$nick_name)){
-	$_SESSION['msg'] = ['error' => ['ニックネームを入力してください。']];
+	$_SESSION['msg']['error'][] = 'ニックネームを入力してください。';
 }
 
 //性別 NG >> 未選択
 if(!isset($gender)){
-    $_SESSION['msg'] = ['error' => ['性別を選んでください。']];
+    $_SESSION['msg']['error'][] = '性別を選んでください。';
 }
 
 //生年月日 NG >> 未選択
 if($birth == '' || mb_ereg_match('^(\s|　)+$',$birth)){
-	$_SESSION['msg'] = ['error' => ['生年月日を入力してください。']];
+	$_SESSION['msg']['error'][] = '生年月日を入力してください。';
 }
 
 //パスワード NG >> 1回目空欄・2回目空欄・不一致・半角もしくは8文字以上でない
 if($pass == '' || mb_ereg_match('^(\s|　)+$',$pass)){
-	$_SESSION['msg'] = ['error' => ['パスワードを入力してください。']];
+	$_SESSION['msg']['error'][] = 'パスワードを入力してください。';
 }elseif($pass2 == '' || mb_ereg_match('^(\s|　)+$',$pass2)){
-	$_SESSION['msg'] = ['error' => ['2回目のパスワードを入力してください。']];
+	$_SESSION['msg']['error'][] = '2回目のパスワードを入力してください。';
 }elseif($pass !== $pass2){
-	$_SESSION['msg'] = ['error' => ['パスワードが一致しません。']];
+	$_SESSION['msg']['error'][] = 'パスワードが一致しません。';
 }
 
 if(preg_match("/^[a-zA-z0-9]|[!\"#<=>&~@%+$\'\*\^\(\)\[\]\|\/\.,_-]+$/",$pass)==false
@@ -63,7 +59,7 @@ if(preg_match("/^[a-zA-z0-9]|[!\"#<=>&~@%+$\'\*\^\(\)\[\]\|\/\.,_-]+$/",$pass)==
 	|| strlen($pass)<8
 	|| strlen($pass2)<8
 	){
-		$_SESSION['msg'] = ['error' => ['パスワードは半角英数字8桁以上で入力してください。']];
+		$_SESSION['msg']['error'][] = 'パスワードは半角英数字8桁以上で入力してください。';
 	}
 
 $users = null;
