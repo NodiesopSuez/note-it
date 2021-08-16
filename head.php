066e1b3009eca90aaa6ef45be7c093421a04f8aa
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-08-16 13:41:43 +0900

    jquery入れ直す

--- head.php
+++ head.php
@@ -18,3 +18,4 @@
         })(document);
     </script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
